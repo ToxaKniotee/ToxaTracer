1f946d66335081d7c371f785ae791d16a2d5b20f
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-08-29 09:40:21 -0500

    Se creó el sistema de logueo de usuarios.
    Se le permite loguearse con su cuenta de Google, se cambio el nombre de login.html por register.html, en un futuro se tiene que cambiar el nombre de login.py por register.py.

--- login.py
+++ login.py
@@ -2,11 +2,23 @@ import webapp2
 import jinja2
 import os
 
+from google.appengine.api import users
+
 template_env = jinja2.Environment( loader = jinja2.FileSystemLoader( os.getcwd() ) )
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		template = template_env.get_template("login.html")
-		self.response.out.write(template.render())
+		user = users.get_current_user()
+		login_url = users.create_login_url(self.request.path)
+		logout_url = users.create_logout_url(self.request.path)
+
+		context = {
+			"user": user,
+			"login_url": login_url,
+			"logout_url": logout_url,
+		}
+
+		template = template_env.get_template("register.html")
+		self.response.out.write(template.render(context))
 
 application = webapp2.WSGIApplication( [("/", MainPage)], debug=True )
