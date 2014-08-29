280dd420cf14ad2103becac31e6278239d381a29
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-08-29 09:20:27 -0500

    Se creo el login

---
+++ login.py
@@ -0,0 +1,12 @@
+import webapp2
+import jinja2
+import os
+
+template_env = jinja2.Environment( loader = jinja2.FileSystemLoader( os.getcwd() ) )
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		template = template_env.get_template("login.html")
+		self.response.out.write(template.render())
+
+application = webapp2.WSGIApplication( [("/", MainPage)], debug=True )
