7dce36ed31d7a95926611ba5f039bd7d758cf320
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-12-01 02:37:56 -0600

    Se agregaron las interfaces de daniel

--- main.py
+++ main.py
@@ -5,156 +5,240 @@ import models
 import time
 from gaesessions import get_current_session
 
-template_env = jinja2.Environment( loader = jinja2.FileSystemLoader( os.getcwd() ) )
+TEMPLATE_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getcwd()))
 
 class IndexPage(webapp2.RequestHandler):
+    def get(self):
+        render_page(self, "login.html")
 
-	def get(self):
-		renderPage( self, "index.html" )
+    def post(self):
+        # Obtenemos el email y el password
+        email = self.request.get("email_login")
+        password = self.request.get("pass_login")
 
-class LoginPage(webapp2.RequestHandler):
+        # Nos aseguramos de que no son vacios y mandamos a llamar a la funcion para validar los
+        # datos
+        if email != "" and password != "" and models.login_user(email, password):
+            self.redirect("/main")
+            return
 
-	def get(self):
-		renderPage( self, "login.html" )
-
-	def post(self):
-		username = self.request.get("name")
-		password = self.request.get("pass")
-
-		if( username != "" and password != "" ) and models.login_user(username, password):
-			self.redirect( "/main" )
-			return
-
-		renderPage( self, "login.html", {"message": "Usuario o password incorrectos"} )
+        # Datos invalidos o error
+        render_page(self, "login.html", {"error_message": True})
 
 class RegisterPage(webapp2.RequestHandler):
+    def get(self):
+        render_page(self, "register.html")
 
-	def get(self):
-		renderPage( self, "register.html" )
+    def post(self):
+        username = self.request.get("name_register")
+        email = self.request.get("email_register")
+        password = self.request.get("password_register")
+        password_c = self.request.get("password_confirmation_register")
 
-	def post( self ):
-		username   = self.request.get( "name" )
-		email      = self.request.get( "email" )
-		password   = self.request.get( "pass" )
-		password_c = self.request.get( "pass_c" )
+        # Checamos que no eista el mismo email
+        if models.check_email(email):
+            render_page(self, "register.html", {
+                "class": "danger",
+                "message": "Email already exists"})
+            return
 
-		if( not models.check_username( username ) ):
-			renderPage( self, "register.html", {"message": "El nombre de usuaio ya existe"} )
-			return
+        # Checamos que las contrasenias sean las mismas
+        if password == password_c:
+            # Insertamos el usuario
+            models.insert_user(username, email, password)
+            render_page(self, "register.html", {
+                "class": "success",
+                "message": "Successful registration!"})
+            return
 
-		if( not models.check_email( email ) ):
-			renderPage( self, "register.html", {"message": "El Email ya existe"} )
-			return
+        render_page(self, "register.html", {
+            "class": "danger",
+            "message": "An unknow error has occurred"})
 
-		if password == password_c:
-			models.insert_user( username, email, password )
-			self.response.out.write( "Registro Exitoso" )
-			return
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # Obtenemos el usuario que se acaba de registrar y lo regresamos
+        global_user = models.get_global_user()
+        render_page(self, "projects.html", {"user": global_user})
 
-		renderPage( self, "register.html", {"message": "Un error a ocurrido"} )
+    def post(self):
+        action = self.request.get("action")
+        print "post"
 
-class RegisterProject( webapp2.RequestHandler ):
+        # Agregar Proyecto
+        if action == "add_project":
+            project_name = self.request.get("name_project")
+            project_description = self.request.get("description_project")
+            project_start_date = self.request.get("input_start_date_project")
+            project_end_date = self.request.get("input_completition_date_project")
+            project_client = self.request.get("client_project")
+            project_arq = self.request.get("architecture_project")
+            project_lang = self.request.get("language_project")
+            project_owner = self.request.get("owner_project")
 
-	def get( self ):
-		renderPage( self, "register-project.html" )
+            # Agregamos el proyecto
+            project_key = models.add_project(project_name, project_description, project_start_date,
+                project_end_date, project_client, project_owner, project_arq, project_lang)
+            self.redirect("/project?project=" + str(project_key))
+            return
+        # Agregar arquitectura
+        elif action == "add_arq":
+            models.add_arq(self.request.get("architecture_project"))
+            self.redirect("/main#architecture_section")
+        # Agregar lenguaje
+        elif action == "add_language":
+            models.add_language(self.request.get("language_project"))
+            self.redirect("/main#language_section")
 
-	def post( self ):
-		project_name        = self.request.get("project_name")
-		project_description = self.request.get("project_description")
-		project_start_date  = self.request.get("project_start_date")
-		project_end_date    = self.request.get("project_end_date")
-		project_client      = self.request.get("project_client")
-		project_arq         = self.request.get("project_arq")
-		project_lang        = self.request.get("project_lang")
+class ProjectPage(webapp2.RequestHandler):
+    def get(self):
+        project = models.get(self.request.get("project"))
 
-		if models.check_projects( project_name ):
-			if models.add_project( project_name, project_description, project_start_date, project_end_date, project_client, project_arq, project_lang ):
-				self.redirect( "/req" )
-				return
-			self.response.out.write( "Error" )
+        # Una espera insignificativa para que le de tiempo a la base de datos de actualizar la
+        # informacion
+        time.sleep(0.1)
 
-class MainPage( webapp2.RequestHandler ):
+        context = {"project" : project}
+        render_page(self, "project.html", context)
 
-	def get( self ):
-		session = get_current_session()
-		global_user = session.get( "global_user" )
-		projects = models.return_projects();
+    def post(self):
+        project = models.get(self.request.get("project"))
+        action = self.request.get("action")
+        post = ""
 
-		renderPage( self, "main.html", {"Username": global_user.user_name, "projects": projects} )
+        # Agregar requerimientos no funcionales
+        if action == "add_requirement":
+            project.no_func_req.append(self.request.get("requirement_project"))
+            post = "#requirements_section"
+        # Eliminar requerimientos no funcionales
+        elif action == "remove_requirement":
+            index = int(self.request.get("index"))
+            del project.no_func_req[index]
+            post = "#requirements_section"
+        # Agregar Rol
+        elif action == "add_role":
+            email = self.request.get("email_rol_project")
+            role = self.request.get("name_rol_project")
+            models.add_role(email, project, role)
+            post = "#roles_section"
+        # Eliminar rol
+        elif action == "remove_role":
+            models.delete_role(self.request.get("index"))
+            post = "#roles_section"
+        # Agregar Release
+        elif action == "add_release":
+            name = self.request.get("name_release")
+            description = self.request.get("description_release")
+            release_date = self.request.get("input_deadline_release")
+            percentage = self.request.get("percentage_release")
+            models.add_release(name, description, release_date, percentage, project)
+            post = "#releases_section"
 
-class ReqPage( webapp2.RequestHandler ):
+        project.put()
+        self.redirect("/project?project=" + str(project.key()) + post)
 
-	def get( self ):
-		session = get_current_session()
-		global_project = session.get( "global_project" )
+class ReleasePage(webapp2.RequestHandler):
+    def get(self):
+        release = models.get(self.request.get("release"))
+        context = {"release": release}
 
-		#Se elimina el requerimiento
-		v = self.request.get( "v" )
-		if v:
-			del global_project.no_func_req[int( v ) - 1]
-			global_project.put()
-			session["global_project"] = global_project
-			self.redirect( "/req" )
-			return
+        # Una espera insignificativa para que le de tiempo a la base de datos de actualizar la
+        # informacion
+        time.sleep(0.1)
 
-		# Se elimina un rol
-		d = self.request.get( "d" )
-		if d:
-			models.delete_role( d )
-			self.redirect( "/req" )
-			return
+        render_page(self, "release.html", context)
 
-		# Una espera insignificativa para que le de tiempo a la base de datos de actualizar la informacion
-		time.sleep( 0.1 )
+    def post(self):
+        release = models.get(self.request.get("release"))
+        action = self.request.get("action")
+        post = ""
 
-		context = {
-			"name": global_project.name,
-			"req": global_project.no_func_req,
-			"roles": global_project.roles,
-			"releases": global_project.releases.order( "release_date" )
-		}
+        # Agregar una historia de usuario
+        if action == "add_user_story":
+            name = self.request.get("name_story")
+            priority = self.request.get("priority_story")
+            required_days = self.request.get("days_story")
+            engineer = self.request.get("responsible_story")
+            start_date = self.request.get("input_start_date_story")
+            end_date = self.request.get("input_completition_date_story")
+            percentage = self.request.get("percentage_story")
+            models.add_user_story(
+                name, priority, required_days, engineer, start_date, end_date, percentage, release)
+            post = "#user_histories_section"
+        # Agregar requerimiento
+        elif action == "add_requirement":
+            release.requirements.append(self.request.get("requirements_description_release"))
+            release.put()
+            post = "#portafolio"
+        # Eliminar requerimiento
+        elif action == "remove_requirement":
+            index = int(self.request.get("index")) - 1
+            del release.requirements[index]
+            release.put()
+            post = "#portafolio"
 
-		renderPage( self, "project-req.html", context )
+        self.redirect("/release?release=" + str(release.key()) + post)
 
-	def post( self ):
-		session = get_current_session()
-		global_project = session.get( "global_project" )
-		type_post = self.request.get( "type_post" )
+class UserStoryPage(webapp2.RequestHandler):
+    def get(self):
+        user_story = models.get(self.request.get("user_story"))
+        context = {"user_story": user_story}
 
-		# Requerimientos no funcionales
-		if type_post == "type_req":
-			global_project.no_func_req.append( self.request.get( "txt_add_req" ) )
+        # Una espera insignificativa para que le de tiempo a la base de datos de actualizar la
+        # informacion
+        time.sleep(0.1)
 
-		# Roles
-		if type_post == "type_role":
-			if models.check_user( self.request.get( "txt_user" ) ):
-				models.add_role( self.request.get( "txt_user" ), global_project, self.request.get( "txt_role" ) )
-			else:
-				context = {
-					"err_role": "El usuario no existe, tiene que ser un usuario registrado",
-					"name": global_project.name,
-					"req": global_project.no_func_req,
-					"roles": global_project.roles
-				}
-				renderPage( self, "project-req.html", context )
-				return
+        render_page(self, "story.html", context)
 
-		#Releases
-		if type_post == "release":
-			temp_name = self.request.get( "name" )
-			temp_description = self.request.get( "description" )
-			temp_release_date = self.request.get( "release_date" )
-			temp_percentage = self.request.get( "percentage" )
+    def post(self):
+        user_story = models.get(self.request.get("user_story"))
+        action = self.request.get("action")
 
-			models.add_release( temp_name, temp_description, temp_release_date, temp_percentage, global_project )
-			self.redirect( "/req" )
+        # Agregamos una nueva tarea
+        if action == "add_task":
+            name = self.request.get("name_task")
+            description = self.request.get("description_task")
+            priority = self.request.get("priority_task")
+            start_date = self.request.get("input_start_date_task")
+            required_hours = self.request.get("hours_task")
+            percentage = self.request.get("percentage_task")
+            models.add_task(
+                name, description, priority, start_date, required_hours, percentage, user_story)
 
-		global_project.put()
-		session["global_project"] = global_project
-		self.redirect( "/req" )
+        self.redirect("/story?user_story=" + str(user_story.key()))
 
-def renderPage( app, web_page, context = {} ):
-	template = template_env.get_template( "html/" + web_page )
-	app.response.out.write( template.render( context ) )
+class TaskPage(webapp2.RequestHandler):
+    def get(self):
+        task = models.get(self.request.get("task"))
 
-application = webapp2.WSGIApplication( [ ("/", IndexPage), ("/login", LoginPage), ( "/register", RegisterPage ), ( "/register-project", RegisterProject ), ( "/main", MainPage ), ( "/req", ReqPage ) ], debug=True )
+        # Una espera insignificativa para que le de tiempo a la base de datos de actualizar la
+        # informacion
+        time.sleep(0.2)
+
+        context = {"task": task}
+        render_page(self, "task.html", context)
+
+    def post(self):
+        task = models.get(self.request.get("task"))
+        action = self.request.get("action")
+
+        # Agregar test
+        if action == "add_test":
+            name = self.request.get("name_test")
+            models.add_test(name, task)
+
+        self.redirect("/task?task=" + str(task.key()))
+
+def render_page(page_app, web_page, context=None):
+    # Inicializamos el contexto en caso de ser vacio
+    if context is None:
+        context = {}
+    template = TEMPLATE_ENV.get_template("assets/html/" + web_page)
+    page_app.response.out.write(template.render(context))
+
+APP = webapp2.WSGIApplication(
+    [
+        ("/", IndexPage), ("/register", RegisterPage), ("/main", MainPage),
+        ("/project", ProjectPage), ("/release", ReleasePage), ("/story", UserStoryPage),
+        ("/task", TaskPage)],
+    debug=True)
--- models.py
+++ models.py
@@ -2,132 +2,225 @@ from google.appengine.ext import db
 from gaesessions import get_current_session
 import datetime
 
+def get(key):
+    return db.get(key)
+
+####################################################################################################
+## User                                                                                           ##
+####################################################################################################
+
 class User(db.Model):
-	user_name = db.StringProperty()
-	password  = db.StringProperty()
-	email     = db.StringProperty()
+    name = db.StringProperty()
+    password = db.StringProperty()
+    email = db.StringProperty()
+    arq = db.StringListProperty()
+    languages = db.StringListProperty()
+
+# Funcion que checa si ya existe un email en la base de datos
+def check_email(email):
+    query = db.Query(User).filter("email", email)
+    user = query.get()
+    if not user:
+        return False
+    return True
+
+# Funcion que inserta un nuevo usuario a la base de datos
+def insert_user(name, email, password):
+    temp_user = User()
+    temp_user.name = name
+    temp_user.password = password
+    temp_user.email = email
+    temp_user.put()
+
+# Funcion que checa si los datos son correctos
+def login_user(email, password):
+    # Obtenemos el email y checamos que exista
+    query = db.Query(User).filter("email", email)
+    if not query:
+        return False
+
+    # Obtenemos la contrasenia y nos aseguramos de que exista
+    query = query.filter("password", password)
+    if not query:
+        return False
+
+    # Nos aseguramos de que solo exista un valor
+    if query.count() != 1:
+        return False
+
+    # Guardamos el el session el usuario y regresamos True
+    set_global_user(query[0])
+    return True
+
+def get_global_user():
+    session = get_current_session()
+    return session.get("global_user")
+
+def set_global_user(user):
+    session = get_current_session()
+    session["global_user"] = user
+
+def add_arq(new_arq):
+    user = get_global_user()
+    user.arq.append(new_arq)
+    user.put()
+    set_global_user(user)
+
+def add_language(new_language):
+    user = get_global_user()
+    user.languages.append(new_language)
+    user.put()
+    set_global_user(user)
+
+####################################################################################################
+## Projects                                                                                       ##
+####################################################################################################
 
 class Project(db.Model):
-	name        = db.StringProperty()
-	description = db.StringProperty()
-	start_date  = db.DateProperty()
-	end_date    = db.DateProperty()
-	client      = db.StringProperty()
-	user        = db.ReferenceProperty(User, collection_name = "projects")
-	no_func_req = db.StringListProperty()
+    name = db.StringProperty()
+    user = db.ReferenceProperty(User, collection_name="projects")
+    description = db.StringProperty()
+    start_date = db.DateProperty()
+    end_date = db.DateProperty()
+    client = db.StringProperty()
+    no_func_req = db.StringListProperty()
+    owner = db.StringProperty()
+    percentage = db.IntegerProperty()
+    arq = db.StringProperty()
+    language = db.StringProperty()
 
-class Roles( db.Model ):
-	user = db.ReferenceProperty( User, collection_name = "works" )
-	project = db.ReferenceProperty( Project, collection_name = "roles" )
-	role = db.StringProperty()
+def add_project(project_name, project_description, project_start_date, project_end_date,
+        project_client, project_owner, project_arq, project_lang):
+    # Obtenemos el usuario
+    global_user = get_global_user()
 
-class Release( db.Model ):
-	name         = db.StringProperty()
-	description  = db.StringProperty()
-	release_date = db.DateProperty()
-	percentage   = db.IntegerProperty()
-	project      = db.ReferenceProperty( Project, collection_name = "releases" )
+    # Creamos el projecto
+    temp_project = Project()
+    temp_project.description = project_description
+    temp_project.name = project_name
+    temp_project.client = project_client
+    temp_project.owner = project_owner
+    temp_project.user = global_user
+    temp_project.start_date = datetime.datetime.strptime(project_start_date, "%d-%m-%Y").date()
+    temp_project.end_date = datetime.datetime.strptime(project_end_date, "%d-%m-%Y").date()
+    temp_project.no_func_req = []
+    temp_project.arq = project_arq
+    temp_project.language = project_lang
+    temp_project.percentage = 0
+    temp_project.put()
 
-def add_release( name, description, release_date, percentage, project ):
-	temp_release = Release()
-	temp_release.name = name
-	temp_release.description = description
-	temp_release.release_date = datetime.datetime.strptime( release_date, "%Y-%m-%d" ).date()
-	temp_release.percentage = int( percentage )
-	temp_release.project = project
-	temp_release.put();
+    # Regresamos el key del proyecto
+    return temp_project.key()
 
-def add_role( user_email, project, role ):
-	#recuperamos el usuario
-	q = db.Query( User ).filter( "email", user_email )
-	q.get()
-	user = q[0]
+def get_global_project():
+    session = get_current_session()
+    return session.get("global_project")
 
-	#Insertamos el nuevo rol
-	temp_role = Roles()
-	temp_role.user = user
-	temp_role.project = project
-	temp_role.role = role
-	temp_role.put()
+####################################################################################################
+## Role                                                                                           ##
+####################################################################################################
 
-def delete_role( key ):
-	temp_role = db.get( key )
-	temp_role.delete()
+class Role(db.Model):
+    email = db.StringProperty()
+    project = db.ReferenceProperty(Project, collection_name="roles")
+    role = db.StringProperty()
 
-def check_user( email ):
-	q = db.Query( User ).filter( "email", email )
-	user = q.get()
-	if not user:
-		return False
-	return True
+def add_role(user_email, project, role):
+    #Insertamos el nuevo rol
+    temp_role = Role()
+    temp_role.email = user_email
+    temp_role.project = project
+    temp_role.role = role
+    temp_role.put()
 
-def add_project(project_name, project_description, project_start_date, project_end_date, project_client, project_arq, project_lang):
-	session = get_current_session()
-	global_user = session.get("global_user", 0)
-	if not global_user:
-		return False
-	temp_project             = Project()
-	temp_project.description = project_description
-	temp_project.name        = project_name
-	temp_project.client      = project_client
-	temp_project.user        = global_user
-	temp_project.start_date  = datetime.datetime.strptime( project_start_date, "%Y-%m-%d" ).date()
-	temp_project.end_date    = datetime.datetime.strptime( project_end_date, "%Y-%m-%d" ).date()
-	temp_project.no_func_req = []
-	temp_project.put()
-	session = get_current_session()
-	global_project = session.get( "global_project" )
-	session["global_project"] = temp_project
-	return True
+def delete_role(key):
+    temp_role = db.get(key)
+    temp_role.delete()
 
-def return_projects():
-	session = get_current_session()
-	global_user = session.get("global_user", 0)
-	q = db.Query(Project).filter("user", global_user)
-	q.get()
-	return q
+####################################################################################################
+## Release                                                                                        ##
+####################################################################################################
 
-def check_projects(project_name):
-	session = get_current_session()
-	global_user = session.get("global_user", 0)
-	q = db.Query(Project).filter("user", global_user.user_name)
-	q = q.filter("name", project_name)
-	result = q.get()
-	if not result:
-		return True
-	return False
+class Release(db.Model):
+    name = db.StringProperty()
+    description = db.StringProperty()
+    release_date = db.DateProperty()
+    percentage = db.IntegerProperty()
+    requirements = db.StringListProperty()
+    project = db.ReferenceProperty(Project, collection_name="releases")
 
-def check_username(user_id):
-	q = db.Query(User).filter("user_name", user_id)
-	player = q.get()
-	if not player:
-		return True
-	return False
+def add_release(name, description, release_date, percentage, project):
+    temp_release = Release()
+    temp_release.name = name
+    temp_release.description = description
+    temp_release.release_date = datetime.datetime.strptime(release_date, "%d-%m-%Y").date()
+    temp_release.percentage = int(percentage)
+    temp_release.project = project
+    temp_release.put()
 
-def check_email(user_email):
-	q = db.Query(User).filter("email", user_email)
-	player = q.get()
-	if not player:
-		return True
-	return False
+####################################################################################################
+## User's Stories                                                                                 ##
+####################################################################################################
 
-def insert_user(user_name, email, password):
-	temp_user = User()
-	temp_user.user_name = user_name
-	temp_user.password = password
-	temp_user.email = email
-	temp_user.put()
+class UserStory(db.Model):
+    name = db.StringProperty()
+    priority = db.IntegerProperty()
+    required_days = db.IntegerProperty()
+    engineer = db.StringProperty()
+    start_date = db.DateProperty()
+    end_date = db.DateProperty()
+    percentage = db.IntegerProperty()
+    release = db.ReferenceProperty(Release, collection_name="user_stories")
 
-def login_user(username, password):
-	q = db.Query(User).filter("user_name", username)
-	if not q:
-		return False
-	q = q.filter("password", password)
-	if not q:
-		return False
-	if q.count() != 1:
-		return False
-	session = get_current_session()
-	global_user = session.get("global_user", 0)
-	session["global_user"] = q[0]
-	return True
+def add_user_story(name, priority, required_days, engineer, start_date, end_date, percentage, release):
+    temp_story = UserStory()
+    temp_story.name = name
+    temp_story.priority = int(priority)
+    temp_story.required_days = int(required_days)
+    temp_story.engineer = engineer
+    temp_story.start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y").date()
+    temp_story.end_date = datetime.datetime.strptime(end_date, "%d-%m-%Y").date()
+    temp_story.percentage = int(percentage)
+    temp_story.release = release
+    temp_story.put()
+    return temp_story
+
+####################################################################################################
+## Tasks                                                                                          ##
+####################################################################################################
+
+class Task(db.Model):
+    name = db.StringProperty()
+    description = db.StringProperty()
+    priority = db.IntegerProperty()
+    start_date = db.DateProperty()
+    required_hours = db.IntegerProperty()
+    percentage = db.IntegerProperty()
+    user_story = db.ReferenceProperty(UserStory, collection_name="tasks")
+
+def add_task(name, description, priority, start_date, required_hours, percentage, user_story):
+    temp_task = Task()
+    temp_task.name = name
+    temp_task.description = description
+    temp_task.priority = int(priority)
+    temp_task.start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y").date()
+    temp_task.required_hours = int(required_hours)
+    temp_task.percentage = int(percentage)
+    temp_task.user_story = user_story
+    temp_task.put()
+    return temp_task
+
+####################################################################################################
+## Tests                                                                                          ##
+####################################################################################################
+
+class Test(db.Model):
+    name = db.StringProperty()
+    task = db.ReferenceProperty(Task, collection_name="tests")
+
+def add_test(name, task):
+    temp_test = Test()
+    temp_test.name = name
+    temp_test.task = task
+    temp_test.put()
+    return temp_test
